cb88d9da381be715bb8cc6e7a1161988bd2aa65f
AnhLDP <leducphananh@gmail.com>
2026-05-14 15:53:29 +0700

    Fix exit animation opacity in Layout component

--- components/layout.tsx
+++ components/layout.tsx
@@ -108,7 +108,7 @@ const Layout = ({ children }: PropsWithChildren) => {
             key={location.pathname}
             initial={{ opacity: 0, y: 10 }}
             animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: -10 }}
+            exit={{ opacity: 1, y: -10 }}
             transition={{ duration: 0.2 }}>
             {children}
           </motion.div>
