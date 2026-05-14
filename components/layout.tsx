ae53f6d5b540f675c0b79f2494c8df0af34bded5
AnhLDP <leducphananh@gmail.com>
2026-05-14 15:50:50 +0700

    Fix animation key reference and downgrade motion dependency version

--- components/layout.tsx
+++ components/layout.tsx
@@ -105,7 +105,7 @@ const Layout = ({ children }: PropsWithChildren) => {
       <main className="flex-1 pt-14 pb-16 md:pb-0">
         <AnimatePresence mode="wait">
           <motion.div
-            key={pathname}
+            key={location.pathname}
             initial={{ opacity: 0, y: 10 }}
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: -10 }}
